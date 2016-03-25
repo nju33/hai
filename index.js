ebae19933c8740e8f92114b21e839643210bbe86
nju33 <jun.sasaki@mvrck.co.jp>
2016-03-25 18:56:37 +0900

    Commit

--- index.js
+++ index.js
@@ -1,98 +1,56 @@
-var hai = (function() {
-  'use strict';
+let handle;
+const cache = {};
 
-  var component = null,
-    positiveBtn = null,
-    negativeBtn = null,
-    offsetTop = 0,
-    offsetLeft = 0,
-    eventStore = {};
+const handler = {
+  exec: function exec(id, cap, btns) {
+    const id = document.getElementById(id);
+    if (id) {
+      throw Error(`Not found id: ${id}`);
+    }
 
-  var calcOffset = function(elem) {
-    return {
-      left: elem.offsetLeft - ((component.clientWidth - elem.clientWidth) / 2),
-      top: elem.offsetTop + component.clientHeight
-    };
-  };
+    cache[id] = this.generate(cap, btns);
+  },
+  add: function(cap, btns) {
+    const el = generateDOM(cap, btns);
+    document.getElementsByTagName('body')[0].appendChild(el);
+  },
+}
 
-  var show = function(e) {
-    var target = e.currentTarget,
-      transform = window.getComputedStyle(target).transform,
-      offset = calcOffset(target);
 
-    component.style.position = 'absolute';
-    component.style.left = offset.left + 'px';
-    component.style.top = offset.top + 'px';
+export default function hai(id, cap, ...btns) {
+  if (btns.length) {
+    throw Error('err');
+  }
 
-  };
-
-  var createElem = function(title, positiveText, negativeText) {
-    var div = document.createElement('div');
-    div.innerHTML =
-      '<div id="a">' + title + '</div>' +
-      '<div>' +
-        '<button>' + positiveText + '</button>' +
-        '<button>' + negativeText + '</button>' +
-      '</div>';
-    var btns = div.querySelectorAll('button');
-    positiveBtn = btns[0];
-    negativeBtn = btns[1];
-    return div;
-  };
-
-  var insertElem = function(elem) {
-    document.body.appendChild(elem);
-  };
-
-  var registerEvent = function(elem) {
-    elem.addEventListener('click', show);
-    positiveBtn.addEventListener('click', function() {
-      if (eventStore.positiveClick != null)
-        eventStore.positiveClick();
-    });
-    negativeBtn.addEventListener('click', function() {
-      if (eventStore.negativeClick != null)
-        eventStore.negativeClick();
-    });
-  };
-
-  var unregisterEvent = function(elem) {
-    elem.removeEventListener('click', clickHandler);
-  };
+  setTimeout(handler.exec.bind(handler, id, cap, btns), 0);
 
   return {
-    create: function(opts) {
-      var self = this;
-      component = createElem(opts.title, opts.positiveText, opts.negativeText);
-      insertElem(component);
-      return this;
-    },
-    bind: function(opts) {
-      var elems = document.querySelectorAll(opts.element)
-      registerEvent(elems[0]);
-      return this;
-    },
-    on: (function() {
-      var methods = [
-        function(eventMap) {
-          var keys = Object.keys(eventMap);
-          for(var i = 0, len = keys.length; i < len; i++) {
-            var eventName = keys[i],
-              cb = eventMap[eventName];
-            eventStore[eventName] = cb;
-          }
-        },
-        function(eventName, cb) {
-          eventStore[eventName] = cb
-        }
-      ];
-      return function() {
-        if (arguments.length === 1) {
-          methods[0].apply(this, arguments);
-        } else if (arguments.length === 2) {
-          methods[1].apply(this, arguments);
-        }
-      };
-    })()
+    then(_handle) {
+      handle = _handle;
+    }
   };
-})();
+}
+
+function generateDOM(cap, btns) {
+  const templateBtn = (function() {
+    let result = '';
+
+    for (let i = 0, len = btns.length; i < len; i++) {
+      result += `
+        <a>${btns[i]}</a>
+      `;
+    }
+  })();
+  const templateAll = `
+    <div class="hai__box">
+      <div class="hai__header">
+        <span class="hai__cap">${cap}</span>
+      </div>
+      <div class="hai__body">
+      </div>
+    </div>
+  `;
+  const hai = document.createElement('div');
+  hai.outerHTML = templateAll;
+  return hai;
+}
